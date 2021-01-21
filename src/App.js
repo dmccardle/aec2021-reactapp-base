c69f96b7f51c99886453d4bcdd2af8813004cfdc
dmccardle <danny.mccardle@unb.ca>
2021-01-21 19:50:15 -0400

    Added call to backend on button press

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button';
+import { useState } from 'react'
 
 function App() {
+  const [data, setData] = useState('')
+
+  const fetchData = async () => {
+    const res = await fetch('http://localhost:5000')
+    const newData = await res.json()
+
+    setData(newData)
+  }
+
   return (
-    <Button variant="contained" color="primary">
+    <div>
+      <Button variant="contained" color="primary" onClick={fetchData} >
       Hello World
-    </Button>
+      </Button>
+      <label>{data}</label>
+    </div>
   );
 }
 
